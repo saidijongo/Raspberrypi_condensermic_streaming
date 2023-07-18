b235a42d16765c8c795b97d556080613f9632a43
Jongo Said (김정고) <31678025+saidijongo@users.noreply.github.com>
2023-07-18 11:58:29 +0900

    Add files via upload

---
+++ raspiaudio.py
@@ -0,0 +1,33 @@
+import pyaudio
+import numpy as np
+
+# Constants
+FORMAT = pyaudio.paFloat32
+CHANNELS = 2
+RATE = 44100
+CHUNK_SIZE = 1024
+
+# Initialize PyAudio
+audio = pyaudio.PyAudio()
+
+# pyaudio.PyAudio().get_device_info_by_index()
+#pyaudio.PyAudio().get_device_info_by_name()
+
+#audio stream for input and output
+stream = audio.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, output=True, frames_per_buffer=CHUNK_SIZE)
+
+#reading audio data from the microphone and output it to the speaker
+while True:
+    # Read audio data from the input stream
+    input_data = stream.read(CHUNK_SIZE)
+
+    # Processing the audio data
+    processed_data = np.frombuffer(input_data, dtype=np.float32)
+    
+    # Output the processed audio data to the output stream
+    stream.write(processed_data.tobytes())
+
+# Cleanup
+stream.stop_stream()
+stream.close()
+audio.terminate()
